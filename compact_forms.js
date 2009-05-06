c5cf7919478a89f42094b39554df25ff44017900
sun <sun@unleashedmind.com>
2009-05-06 23:43:39 +0000

    #452196 by sun: Fixed JavaScript to re-use existing jQuery objects.

--- compact_forms.js
+++ compact_forms.js
@@ -10,48 +10,48 @@ $.fn.compactForm = function (stars, colons) {
   var colons = colons || 0;
 
   this.each(function (index) {
-    $(this).addClass('compact-form').find("label").each(function () {
-      var label = $(this);
-      var field = $('#' + label.attr('for'));
-      if (field.attr('type') != 'text' && field.attr('type') != 'password') {
+    $(this).addClass('compact-form').find('label').each(function () {
+      var $label = $(this);
+      var $field = $('#' + $label.attr('for'));
+      if ($field.attr('type') != 'text' && $field.attr('type') != 'password') {
         return;
       }
 
-      if ($(field).val() != '') {
-        $(label).fadeOut(1);
+      if ($field.val() != '') {
+        $label.fadeOut(1);
       }
 
-      $(label).parent().addClass('compact-form-wrapper');
-      label.addClass('compact-form-label');
-      field.addClass('compact-form-field');
+      $label.parent().addClass('compact-form-wrapper');
+      $label.addClass('compact-form-label');
+      $field.addClass('compact-form-field');
 
       if (stars === 0) {
-        $(label).find('.form-required').hide();
+        $label.find('.form-required').hide();
       }
       else if (stars === 2) {
-        $(label).find('.form-required').insertAfter(field).prepend('&nbsp;');
+        $label.find('.form-required').insertAfter($field).prepend('&nbsp;');
       }
 
       if (colons === 0) {
-        var lbl = $(label).html();
+        var lbl = $label.html();
         lbl = lbl.replace(/:/,' ');
-        $(label).html(lbl);
+        $label.html(lbl);
       }
 
-      $(field).focus(function () {
+      $field.focus(function () {
         if($(this).val() === '') {
-          $(label).fadeOut('fast');
+          $label.fadeOut('fast');
         }
       });
 
-      $(field).blur(function () {
+      $field.blur(function () {
         if($(this).val() === '') {
-          $(label).fadeIn('slow');
+          $label.fadeIn('slow');
         }
       });
     });
   });
-}
+};
 
 Drupal.behaviors.compactForms = function (context) {
   if (!Drupal.settings || !Drupal.settings.compactForms) {
@@ -60,6 +60,6 @@ Drupal.behaviors.compactForms = function (context) {
   $.each(Drupal.settings.compactForms.forms, function () {
     $('#' + this, context).compactForm(Drupal.settings.compactForms.stars, Drupal.settings.compactForms.colons);
   });
-}
+};
 
 })(jQuery);
