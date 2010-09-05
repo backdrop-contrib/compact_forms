7b4090b29bdf070d56eceb76d3d055b77cd119db
sun <sun@unleashedmind.com>
2010-09-05 01:50:37 +0000

    #461918 by neochief, mstrelan, sun: Fixed Chrome/Safari/Firefox browser quirks with user login form.

--- compact_forms.js
+++ compact_forms.js
@@ -2,6 +2,8 @@
 
 (function ($) {
 
+Drupal.compactForms = {};
+
 /**
  * Compact Forms jQuery plugin.
  */
@@ -21,7 +23,8 @@ $.fn.compactForm = function (stars) {
       }
 
       if ($field.val() != '') {
-        $label.fadeOut(1);
+        // Firefox doesn't like .hide() here for some reason.
+        $label.css('display', 'none');
       }
 
       $label.parent().addClass('compact-form-wrapper');
@@ -36,27 +39,63 @@ $.fn.compactForm = function (stars) {
       }
 
       $field.focus(function () {
-        if($(this).val() === '') {
+        if ($field.val() === '') {
           $label.fadeOut('fast');
         }
       });
 
       $field.blur(function () {
-        if($(this).val() === '') {
+        if ($field.val() === '') {
           $label.fadeIn('slow');
+        }
+      });
+
+      // Chrome adds passwords after page load, so we need to track changes.
+      $field.change(function () {
+        if ($field != document.activeElement) {
+          if ($field.val() === '') {
+            $label.fadeIn('fast');
+          }
+          else {
+            $label.css('display', 'none');
+          }
         }
       });
     });
   });
 };
 
+/**
+ * Attach compact forms behavior to all enabled forms upon page load.
+ */
 Drupal.behaviors.compactForms = {
   attach: function (context, settings) {
     if (!settings || !settings.compactForms) {
       return;
     }
     $('#' + settings.compactForms.forms.join(',#'), context).compactForm(settings.compactForms.stars);
+
+    // Safari adds passwords without triggering any event after page load.
+    // We therefore need to wait a bit and then check for field values.
+    if ($.browser.safari) {
+      setTimeout(Drupal.compactForms.fixSafari, 200);
+    }
   }
 };
+
+/**
+ * Checks for field values and hides the corresponding label if non-empty.
+ *
+ * @todo Convert $.fn.compactForm to always use a function like this.
+ */
+Drupal.compactForms.fixSafari = function () {
+  $('label.compact-form-label').each(function () {
+    var $label = $(this);
+    var context = this.form;
+    if ($('#' + $label.attr('for'), context).val() != '') {
+      $label.css('display', 'none');
+    }
+  });
+}
 
 })(jQuery);
